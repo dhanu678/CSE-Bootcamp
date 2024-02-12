cdf3eab8802844c2aeb22da4dbd2eea69180cde6
Ayush Soni <ayush.soni2464@gmail.com>
2024-02-12 10:12:00 +0530

    type casting and pretty printing

---
+++ day-4/BooleanDemo.java
@@ -0,0 +1,8 @@
+class BooleanDemo {
+    public static void main(String[] args){
+        boolean flag = true;
+        boolean flag2 = false;
+
+        // System.out.println(flag - flag2);
+    }
+}
---
+++ day-4/CharDemo.java
@@ -0,0 +1,22 @@
+public class CharDemo {
+    public static void main(String[] args){
+        char ch1 = '0';
+        char ch2 = '1';
+      //  System.out.println((int)ch1);
+      //  System.out.println((int)ch2);
+       System.out.println("Addition of ch1 and ch2 " + (char)(ch1 + ch2) + " is -> " );
+
+        int a = 1;
+       // System.out.println((boolean)a);
+
+        boolean b = true;
+       // System.out.println((int)b);
+        // pretty printing
+
+        System.out.println("Char ch1 has value -> " + ch1);
+
+
+
+
+    }
+}
